1c5c9daaed26676333dbccbb0ccfe0b4a84eb8c9
SulemanSadat <slemansadat779@gmail.com>
2026-09-16 12:36:34 +0100

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'export',
+  basePath: '/azizi-jewellery',
+  assetPrefix: '/azizi-jewellery/',
+  images: {
+    unoptimized: true,
+  },
+
   allowedDevOrigins: ["192.168.0.197"],
   devIndicators: false,
 };
